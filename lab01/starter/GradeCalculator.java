6000073d4c07fec8304c37127757ec02b9552578
Xinyao90 <lixinyao90@gmail.com>
2026-08-25 15:20:20 -0500

    Update GradeCalculator example

--- lab01/starter/GradeCalculator.java
+++ lab01/starter/GradeCalculator.java
@@ -2,12 +2,28 @@ public class GradeCalculator {
 
     public static double average(int[] scores) {
         // TODO
-        return 0;
+        if (scores == null || scores.length == 0) {
+            throw new IllegalArgumentException("Scores cannot be null or empty.");
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        double average = sum / scores.length;
+        return average;
     }
 
     public static int highest(int[] scores) {
         // TODO
-        return 0;
+        if (scores == null || scores.length == 0) {
+            throw new IllegalArgumentException("scores cannot be null or empty.");
+        }
+        int highest = scores[0];
+        for (int score : scores) {
+            if (score > highest) highest = score;
+        }
+        return highest;
+
     }
 
     public static void main(String[] args) {
