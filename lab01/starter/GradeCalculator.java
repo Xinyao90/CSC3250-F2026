7e348dca4f3a053f3f1e67399576c82df3512fcc
Xinyao90 <lixinyao90@gmail.com>
2026-08-25 14:25:12 -0500

    Add GradeCalculator starter

---
+++ lab01/starter/GradeCalculator.java
@@ -0,0 +1,19 @@
+public class GradeCalculator {
+
+    public static double average(int[] scores) {
+        // TODO
+        return 0;
+    }
+
+    public static int highest(int[] scores) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] scores = {85, 90, 72, 96, 88};
+
+        System.out.println("Average: " + average(scores));
+        System.out.println("Highest: " + highest(scores));
+    }
+}
